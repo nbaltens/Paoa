fca9e535b2592f717d005e420af1b69335457dd2
Noah Baltensperger <nbalten@gmail.com>
2020-02-25 00:45:00 -0600

    Update index.js
    Added delete function to shopping cart

--- index.js
+++ index.js
@@ -81,17 +81,6 @@ function totalCost(product) {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
 function displayCart() {
     let cartItems = localStorage.getItem("productsInCart");
     cartItems = JSON.parse(cartItems);
@@ -109,16 +98,19 @@ function displayCart() {
             productContainer.innerHTML += `
                 <div class="product">
                     <div><img class="cart-product-image" src=${item.itemimg}></div>
-                    <div>${item.itemtitle}</div>
+                    <div class="itemTitleCart">${item.itemtitle}</div>
+                    
                     <div><span>Size: </span>${item.itemsize}</div>
                 <div class="quantity">QTY: ${item.inCart}</div>
-                <div>$${item.inCart * item.itemprice}</div>
+                <div class="itemPriceCart">$${item.inCart * item.itemprice}</div>
+                
                 </div>
              
             `
         });
 
         productContainer.innerHTML += `
+        <button class="clearCart" onClick="clearCart()">Clear Cart</button>
             <div class="cartTotal">
                 <h4 class="basketTotalTitle">
                 TOTAL
@@ -130,6 +122,12 @@ function displayCart() {
             </div>
             `
     }
+
+}
+
+function clearCart() {
+    localStorage.clear()
+    location.reload();
 }
 
 function addToCart(item) {
@@ -162,29 +160,17 @@ onLoadCartNumbers();
 displayCart();
 
 
-
 // MODAL
 
-// Get the modal
 var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
