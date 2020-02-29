bc8082d5fb7d171eb124c549ecc694688cb5d69f
Noah Baltensperger <nbalten@gmail.com>
2020-02-28 22:09:10 -0600

    Update index.js
    added increment and decrement on cart page.

--- index.js
+++ index.js
@@ -19,16 +19,19 @@ for(let i = 0; i < carts.length; i++) {
         addToCart(carts[i])
         setItemInStorage(newObj);
         totalCost(newObj);
+        
     })
 }
 
+// set cart count for cart icon
 function onLoadCartNumbers() {
     let productNumbers = localStorage.getItem('cartNumbers');
     if(productNumbers) {
         document.querySelector('.cartCount').textContent = productNumbers;
     }
+    
 }
-
+// set cart count and local storage count
 function cartNumbers() {
     let productNumbers = localStorage.getItem('cartNumbers');
     productNumbers = parseInt(productNumbers);
@@ -41,6 +44,7 @@ function cartNumbers() {
     }
 }
 
+// increment cart acording to product count
 
 function incrementCart() {
     let productNumbers = localStorage.getItem('cartNumbers');
@@ -52,9 +56,16 @@ function incrementCart() {
     quantityCalc++
     quantityNum.textContent = quantityCalc 
 
+    let cartItemPrice = quantityNum.parentElement.parentElement.querySelector(".itemPriceInCart").textContent;
+    cartItemPrice = parseInt(cartItemPrice)
+    let total = document.querySelector(".basketTotalValue");
+    let totalVal = total.textContent
+    totalVal = parseInt(totalVal)
+    totalVal += cartItemPrice;
+    total.textContent = totalVal 
 }
 
-
+// decremet cart acording to product count
 function decrementCart() {
     let productNumbers = localStorage.getItem('cartNumbers');
     productNumbers = parseInt(productNumbers);
@@ -63,9 +74,18 @@ function decrementCart() {
     let quantityNum = event.target.parentElement.querySelector('.quantityNum')
     let quantityCalc = event.target.parentElement.querySelector('.quantityNum').textContent;
     quantityCalc--
-    quantityNum.textContent = quantityCalc  
-}
+    quantityNum.textContent = quantityCalc 
+    
+    let cartItemPrice = quantityNum.parentElement.parentElement.querySelector(".itemPriceInCart").textContent;
+    cartItemPrice = parseInt(cartItemPrice)
+    let total = document.querySelector(".basketTotalValue");
+    let totalVal = total.textContent
+    totalVal = parseInt(totalVal)
+    totalVal -= cartItemPrice;
 
+    total.textContent = totalVal
+}
+// set items as objects in local storage
 function setItemInStorage(product) {
     let cartItems = localStorage.getItem('productsInCart');
     cartItems = JSON.parse(cartItems);
@@ -87,24 +107,19 @@ function setItemInStorage(product) {
     
 }
 
-
+// set total cost in local storage
 function totalCost(product) {
     let cartCost = localStorage.getItem('totalCost');
     let productPrice = parseInt(product.itemprice)
-    console.log(productPrice)
-    console.log(cartCost)
-
-
     if(cartCost != null) {
         cartCost = parseInt(cartCost);
         localStorage.setItem("totalCost", cartCost + productPrice)
     } else {
         localStorage.setItem("totalCost", product.itemprice);
-
     }
 }
 
-
+// display what is in local storage on Cart HTML page
 function displayCart() {
     let cartItems = localStorage.getItem("productsInCart");
     cartItems = JSON.parse(cartItems);
@@ -118,7 +133,6 @@ function displayCart() {
 
         Object.values(cartItems).map(item => {
         
-            
             productContainer.innerHTML += `
                 <div class="product">
                     <div><img class="cart-product-image" src=${item.itemimg}></div>
@@ -127,7 +141,7 @@ function displayCart() {
                 <div class="quantity"><i class="fas fa-chevron-circle-up upBtn" onClick="incrementCart()"></i><span class="quantityNum" value="${item.inCart}"> ${item.inCart}</span><i class="fas fa-chevron-circle-down downBtn" onClick="decrementCart()"></i></div>
                 
                 
-                <div class="itemPriceCart">$${item.inCart * item.itemprice}</div>
+                <div class="itemPriceCart" value="${item.itemprice}">$<span class="itemPriceInCart">${item.inCart * item.itemprice}</span></div>
                 
                 </div>
              
@@ -140,8 +154,8 @@ function displayCart() {
                 <h4 class="basketTotalTitle">
                 TOTAL
                 </h4>
-                <h4 class="basketTotal">
-                    $${cartCost}.00
+                <h4 class="basketTotal" value="${cartCost}">
+                    $<span class="basketTotalValue">${cartCost}</span>.00
                 </h4>
                 <button class="checkoutBtn" id="myBtn">Checkout</button>
             </div>
@@ -150,11 +164,13 @@ function displayCart() {
 
 }
 
+// clear cart Button function with onclick
 function clearCart() {
     localStorage.clear()
     location.reload();
 }
 
+// create object out of item clicked on
 function addToCart(item) {
     const wholeItem = item.parentElement.parentElement;
     const itemTitle = wholeItem.getElementsByClassName('title')[0].innerText;
@@ -176,6 +192,7 @@ function addToCart(item) {
         }
     }
     newObj = new Itemobj(itemTitle, itemImg, itemPrice, itemSize, tag, inCart);
+    console.log(newObj)
     return newObj;
 }
 
